217dc5e486fa3ff45e11ff52ecfd656386f17311
milon <milonsheikh88@gmail.com>
2022-11-15 12:57:38 +0600

    Flutter Charts Example.

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_line_chart/pie_chart.dart';
+import 'bar_chart.dart';
+import 'line_chart.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Charts',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // home: const LineChartSample(),
+      // home: const PieChartSample(),
+      home: const BarChartSample(),
+    );
+  }
+}
+
